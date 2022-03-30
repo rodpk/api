c412d7c6d2ac9983a77aec8761588a50e1adfeb2
rodpk <rodp.rodrigopinheiro@gmail.com>
2022-03-30 13:33:56 -0300

    user resource and find by id

--- src/main/java/br/com/rodpk/api/ApiApplication.java
+++ src/main/java/br/com/rodpk/api/ApiApplication.java
@@ -3,15 +3,11 @@ package br.com.rodpk.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.rodpk.api.domain.User;
-
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-		User user = new User(1, "Rodrigo", "rodp.rodrigopinheiro@gmail.com", "pass");
 	}
 
 }
