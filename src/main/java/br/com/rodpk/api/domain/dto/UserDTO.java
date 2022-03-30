386fd1b3930d28e38406dd6e3679a49fde3e3620
rodpk <rodp.rodrigopinheiro@gmail.com>
2022-03-30 17:57:25 -0300

    adiciona modelmapper em forma de bean retorna dto

--- src/main/java/br/com/rodpk/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/rodpk/api/domain/dto/UserDTO.java
@@ -1,15 +1,23 @@
 package br.com.rodpk.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
 
     private Integer id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
     
 }
--- src/main/java/br/com/rodpk/api/resources/UserResource.java
+++ src/main/java/br/com/rodpk/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.rodpk.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rodpk.api.domain.User;
+import br.com.rodpk.api.domain.dto.UserDTO;
 import br.com.rodpk.api.services.UserService;
 
 @RestController
@@ -19,9 +21,12 @@ public class UserResource {
     private UserService service;
 
 
+    @Autowired
+    private ModelMapper mapper;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
     
 }
