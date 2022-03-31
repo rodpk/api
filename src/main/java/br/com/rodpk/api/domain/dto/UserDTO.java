a4ca66305c2897352864b66c1ab7b1485c9ffc5c
rodpk <rodp.rodrigopinheiro@gmail.com>
2022-03-31 08:46:02 -0300

    limpando codigo

--- src/main/java/br/com/rodpk/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/rodpk/api/domain/dto/UserDTO.java
@@ -1,10 +1,8 @@
 package br.com.rodpk.api.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
